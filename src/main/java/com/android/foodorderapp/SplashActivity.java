1be2ec9bbcd51c87524c2aa8e4e5bb0dd72ba6f0
Ibrahim297 <85791368+Ibrahim297@users.noreply.github.com>
2022-09-12 20:43:07 +0000

    Android_App_Project
    This is the android app developed by group 41 for DCIT202 project.

---
+++ src/main/java/com/android/foodorderapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.android.foodorderapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+//        This hides the navigation bar on the splash screen
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+//        This will display the first screen for 2s then it will go to the main activity screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
